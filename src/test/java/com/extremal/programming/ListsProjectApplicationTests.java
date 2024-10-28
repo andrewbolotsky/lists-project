744b9dd45b71d38ae20da7562a14f67f7236b019
maksimkaSH <shteinmaks@gmail.com>
2024-10-28 11:12:21 +0300

    init: Initialize Spring MVC

---
+++ src/test/java/com/extremal/programming/ListsProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.extremal.programming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListsProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
